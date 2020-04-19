94b68b4c62ed1489c28e13d9ce9b2cddb088a20d
VJYM27 <ronica27@gmail.com>
2020-04-18 22:58:42 -0400

    first commit

---
+++ page11/index.rb
@@ -0,0 +1,11 @@
+# Import the Date class using require
+require "date"
+
+# Create an instance of Date, then assign it to the birthday variable
+birthday = Date.new(1984,9,27)
+
+# Print the birthday variable
+puts birthday
+
+# Call birthday's sunday? method, then print the return value
+puts birthday.sunday?
